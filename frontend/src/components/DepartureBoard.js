c3a70e77eb0194f085b6282defbd73603f644a6d
blakepercy <blake.percy@resonate.tech>
2019-02-16 16:58:50 +0000

    Extra files needed for a Heroku deployment. Minor bug fix to deal with bus replacement services.

--- frontend/src/components/DepartureBoard.js
+++ frontend/src/components/DepartureBoard.js
@@ -17,21 +17,45 @@ class DepartureBoard extends Component
       .then(response => response.json())
       .then(data => {
         this.setState({ locationName: <div className="Location-header"><h2>{data.getStationBoardResult.locationName}</h2></div> });
-        let services = data.getStationBoardResult.trainServices.service.map((service) => {
-          let type = service.serviceType.toString().toLowerCase();
-          let departureTime = service.std;
-          let punctuality = service.etd.toString().toLowerCase();
-          let platform = service.platform;
-          let destination = service.destination.location.map((location) => {return(location.locationName)});
-          let delayReason = service.delayReason;
-          return(
-              <div className="Left-align Left-padding">
-                <h3>
-                  {departureTime} - {type} to {destination} from platform {platform} is expected {punctuality}. {delayReason}
-                </h3>
-              </div>
-          )
-        });
+
+        let services = null;
+
+        if (data.getStationBoardResult.trainServices != null)
+        {
+          services = data.getStationBoardResult.trainServices.service.map((service) => {
+            let type = service.serviceType.toString().toLowerCase();
+            let departureTime = service.std;
+            let punctuality = service.etd.toString().toLowerCase();
+            let platform = service.platform;
+            let destination = service.destination.location.map((location) => {return(location.locationName)});
+            let delayReason = service.delayReason;
+            return(
+                <div className="Left-align Left-padding">
+                  <h3>
+                    {departureTime} - {type} to {destination} from platform {platform} is expected {punctuality}. {delayReason}
+                  </h3>
+                </div>
+            )
+          });
+        }
+        else if (data.getStationBoardResult.busServices != null)
+        {
+          services = data.getStationBoardResult.busServices.service.map((service) => {
+            let type = service.serviceType.toString().toLowerCase();
+            let departureTime = service.std;
+            let punctuality = service.etd.toString().toLowerCase();
+            let platform = service.platform;
+            let destination = service.destination.location.map((location) => {return(location.locationName)});
+            let delayReason = service.delayReason;
+            return(
+                <div className="Left-align Left-padding">
+                  <h3>
+                    {departureTime} - {type} to {destination} from platform {platform} is expected {punctuality}. {delayReason}
+                  </h3>
+                </div>
+            )
+          });
+        }
         this.setState({ trainServices: services })
       })
       .catch(error => this.setState( {locationName: error.toString()} ));
