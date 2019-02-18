ed6a0a4f51b732a858104457897a8fe7e99ede26
blakepercy <blake.percy@resonate.tech>
2019-02-18 20:12:35 +0000

    Improved the Clock components stateful design

---
+++ frontend/src/components/Clock.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => this.tick(),
+        1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+        <div className="Clock Centre-align">
+          <h3>{this.state.date.toLocaleTimeString()}</h3>
+        </div>
+
+    );
+  }
+}
+
+export default Clock;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,31 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clock from './components/Clock';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-function Clock(props) {
-  return (
-      <div className="Clock Centre-align">
-        <h3>{new Date().toLocaleTimeString()}</h3>
-      </div>
-
-  );
-}
-
-function tick()
-{
-  ReactDOM.render(
-      (
-          <div>
-            <Clock/>
-            <App />
-          </div>
-      ), document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    (
+        <div>
+          <Clock />
+          <App />
+        </div>
+    ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
