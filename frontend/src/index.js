6c50e914f85e7e20feadcb4268bb0df8965f4b52
blakepercy <blake.percy@resonate.tech>
2019-02-18 19:45:17 +0000

    Added a clock Improved component structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+function Clock(props) {
+  return (
+      <div className="Clock Centre-align">
+        <h3>{new Date().toLocaleTimeString()}</h3>
+      </div>
+
+  );
+}
+
+function tick()
+{
+  ReactDOM.render(
+      (
+          <div>
+            <Clock/>
+            <App />
+          </div>
+      ), document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
