64cc428102cf741fe8611541de625642eb014a03
blakepercy <blake.percy@resonate.tech>
2019-02-22 16:01:42 +0000

    Tabulate the departure board

--- frontend/src/components/NreLogo.js
+++ frontend/src/components/NreLogo.js
@@ -18,7 +18,7 @@ class NreLogo extends React.Component {
 
   render() {
     return (
-        <div className="Left-align Left-padding Top-padding">
+        <div className="Right-align Right-padding Top-padding">
           <a href="http://www.nationalrail.co.uk/" target="_blank"
              rel="noopener noreferrer">
             <img src={nreLogo} height={this.height} alt={"National Rail"
