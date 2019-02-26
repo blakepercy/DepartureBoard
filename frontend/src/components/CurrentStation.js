7e5f4536f5b0e0191295e3e95d755483375e6f33
blakepercy <blake.percy@resonate.tech>
2019-02-26 18:50:12 +0000

    Extra stations added

--- frontend/src/components/CurrentStation.js
+++ frontend/src/components/CurrentStation.js
@@ -31,9 +31,15 @@ class CurrentStation extends React.Component {
             <select className="Location-selector" multiple={false} value={this.state.selectedStationCrs} onChange={this.handleChange}>
               <option value="MTB">Matlock Bath</option>
               <option value="DBY">Derby</option>
-              <option value="SLB">Saltburn</option>
-              <option value="MBR">Middlesbrough</option>
+              <option value="AMB">Ambergate</option>
+              <option value="BLP">Belper</option>
+              <option value="DFI">Duffield</option>
               <option value="YRK">York</option>
+              <option value="MBR">Middlesbrough</option>
+              <option value="SLB">Saltburn</option>
+              <option value="WDM">Windermere</option>
+              <option value="STP">London St Pancras</option>
+              <option value="PAD">London Paddington</option>
             </select>
           </form>
         </h2>
